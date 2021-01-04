b6ea791094cc1773bae779785f183ba9dac877fe
Hugo Roca <hugo.rock20@hotmail.com>
2021-01-03 23:08:37 -0500

    ins laravelcollection/html and desing login view

--- resources/views/connect/login.blade.php
+++ resources/views/connect/login.blade.php
@@ -3,4 +3,23 @@
 @section('title', 'login')
 
 @section('content')
+<div class="box box_login shadow">
+    {!! Form::open(['url' => '/login', 'autocomplete' => 'off']) !!}
+    <label for="email">Email:</label>
+    <div class="input-group">
+        <span class="input-group-text">
+            <i class="far fa-envelope-open"></i>
+        </span>
+        {!! Form::email('email', null, ['class' => 'form-control']) !!}
+    </div>
+    <label for="password" class="mtop16">Password:</label>
+    <div class="input-group">
+        <span class="input-group-text">
+            <i class="fas fa-lock"></i>
+        </span>
+        {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
+    {!! Form::submit('Sign in', ['class' => 'btn btn-success mtop16']) !!}
+    {!! Form::close() !!}
+</div>
 @stop
--- resources/views/connect/master.blade.php
+++ resources/views/connect/master.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Cms - @yield('title')</title>
-    <link rel="stylesheet" href="{{ url('/static/css/connect.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ url('/static/css/connect.css?v='.time()) }}">
+    <script src="https://kit.fontawesome.com/297b82e600.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
